dd1e9f45652a9db44126d7ecf7e615193d9785e0
Indira Stepanova <m_indira_i@hotmail.com>
2022-04-11 21:59:36 +0300

    repo changes

--- sber/list_1.py
+++
@@ -1,8 +0,0 @@
-'''Задан список с числами. Напишите программу, которая выводит все элементы списка с четными индексами в виде нового списка.'''
-
-input_list = [int(current) for current in input().split()]
-output_list = []
-for i in range(len(input_list)):
-    if i % 2 == 0:
-        output_list.append(input_list[i])
-print(*output_list)
--- sber/set_intersect.py
+++
@@ -1,4 +0,0 @@
-"""Даны два списка чисел. Напишите программу, которая определяет, сколько в них встречается общих чисел, используя множества."""
-first_set = {str(current) for current in input().split(', ')}
-second_set = {str(current) for current in input().split(', ')}
-print(len(first_set.intersection(second_set)))
